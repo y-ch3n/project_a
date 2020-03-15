17c14d0465735e46b48078b939cecc80c2f30fe6
y-ch3n <yuanchen0318@gmail.com>
2020-03-15 16:52:19 +0800

    change billable from user to company

--- app/User.php
+++ app/User.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable, Billable, HasRoles;
+    use HasApiTokens, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
