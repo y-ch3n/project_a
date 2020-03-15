f30a1b6a6afb2b0553dd9da75e4fe1ab96923e42
y-ch3n <yuanchen0318@gmail.com>
2020-03-15 15:01:48 +0800

    install role permission package

--- app/User.php
+++ app/User.php
@@ -4,13 +4,14 @@ namespace App;
 
 use Laravel\Cashier\Billable;
 use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable, Billable;
+    use HasApiTokens, Notifiable, Billable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
